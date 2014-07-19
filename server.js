ba2504a37b5d306054cd3fbe01b065d89c94e6ae
Morgante Pell <morgante.pell@morgante.net>
2014-07-18 23:49:16 -0400

    retrieve fitbit tokens

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ var express = require('express')
 var pkg = require('./package.json')
 		, main = require('./routes/main')
 
+var Fitbit = require('fitbit');
+var fitbit = new Fitbit(process.env.FITBIT_CONSUMER_KEY, process.env.FITBIT_CONSUMER_SECRET);
+
+var passport = require('passport');
+var FitbitStrategy = require('passport-fitbit').Strategy;
+
 // set up Mongoose
 mongoose.connect('localhost', pkg.name);
 var db = mongoose.connection;
@@ -35,6 +41,28 @@ app.configure(function() {
 
 // set up routes
 app.get('/', main.index);
+
+passport.use(new FitbitStrategy({
+	consumerKey: process.env.FITBIT_CONSUMER_KEY,
+	consumerSecret: process.env.FITBIT_CONSUMER_SECRET,
+	callbackURL: "http://localhost:" + process.env.PORT + "/auth/fitbit/callback"
+}, function(token, tokenSecret, profile, done) {
+	console.log(profile, token, tokenSecret);
+	// done();
+    // User.findOrCreate({ fitbitId: profile.id }, function (err, user) {
+    //   return done(err, user);
+    // });
+  }
+));
+
+// fitbit auth
+app.get('/auth/fitbit', passport.authenticate('fitbit'));
+
+app.get('/auth/fitbit/callback', passport.authenticate('fitbit', { failureRedirect: '/login' }),
+	function(req, res) {
+		// Successful authentication, redirect home.
+		res.redirect('/');
+	});
 
 // start listening
 app.listen( process.env.PORT , function() {
