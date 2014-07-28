d79681448883856ec1100473f354eb8593b4efc0
Morgante Pell <morgante.pell@morgante.net>
2014-07-28 18:05:27 -0400

    play around with react tutorial

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,62 @@
-$(document).ready( function() {
-	_.templateSettings = {
-	  interpolate : /\{\{(.+?)\}\}/g
-	};
-	
-	console.log( 'app ready to go' );
-	
-	$('#prompt .content').html( _.template($('#template-hello').html(), {name: 'Bob'}) );
-	
-	
-	$('#start').click( function() {
-		$('#prompt').slideUp();
-		$('#loading').slideDown();
-		
-		return false;
-	});
+/** @jsx React.DOM */
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
 });
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data} />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+/** @jsx React.DOM */
+React.renderComponent(
+  <CommentBox data={data} />,
+  document.getElementById('content')
+);
+
